710dd0da68ce57292ae3d4bc8610a7dd4d139369
Tolunay70 <31624177+Tolunay70@users.noreply.github.com>
2018-06-25 19:58:26 +0200

    Add files via upload

---
+++ echteportfolio/index.js
@@ -0,0 +1,9 @@
+//
+// var test = false;
+// function delay(){setTimeout(function(){
+//     document.getElementById("cirkel").play();
+// }, 200);
+// test = true}
+// if (test === false) {
+//     delay();
+// }
